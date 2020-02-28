ca2e53f4f65ad3023cb5ac12bcb9fda973f72068
unknown <utkrisht.44.sharma@gmail.com>
2020-02-28 12:41:27 +0530

    added class wrapper; added class Newton; added method Newton.Cosine; added non functional-class JSONUtil.java

--- jnewton/src/main/java/jnewton/App.java
+++ jnewton/src/main/java/jnewton/App.java
@@ -8,6 +8,8 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        //Wrapper.makeRequest("simplify","2*2");
+        System.out.println(Newton.Cosine("pi"));
+        //System.out.println( "Hello World!" );
     }
 }
